081e5f6b5b7bb145f0824b9ee719040cf24e0e87
Juan S. Perales <juansperales@gmail.com>
2020-03-31 15:36:07 +0000

    Clase 2 variables +

--- 2_variables.sh
+++ 2_variables.sh
@@ -6,3 +6,10 @@ nombre=Juan
 
 echo "Opcion: $opcion y Nombre: $nombre"
 
+# Exportar la variable nombre para que este disponible a los demas procesos
+export nombre
+
+# Llamar al siguiente script para recuperar la variable
+
+./2_variables_2.sh
+
