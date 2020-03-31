dea41f2d60728293749256cae8aefa038412dac1
Juan S. Perales <juansperales@gmail.com>
2020-03-31 15:29:28 +0000

    Clase 2 Bash script

---
+++ 1_utilityPostgres.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Programa para realizar algunas operacioens utilitarias de Postgres
+echo "Hola bienvenido al curso de programacion bash"
+
---
+++ 2_variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Programa para revisar la declaracion de variables
+
+opcion=0
+nombre=Juan
+
+echo "Opcion: $opcion y Nombre: $nombre"
+
